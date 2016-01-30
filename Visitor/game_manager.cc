0528f30cf81927bfec10b9e7fd1a228211bcb9b6
Courtney Glosser <glosser_courtney@bah.com>
2016-01-30 15:12:28 -0500

    Updating GameManager to offload input to InputManager - main menu

--- Visitor/game_manager.cc
+++ Visitor/game_manager.cc
@@ -55,46 +55,11 @@ void GameManager::Init() {
 
     display.DisplayWelcome();
 
-    char welcomeInput = input.WelcomeMenuInput();
-
-    cout << "Did I get my input?  " << welcomeInput << endl;
-
-    bool quit = false;
-    SDL_Event e;
-    while (!quit) {
-        //Handle events on queue
-        while( SDL_PollEvent( &e ) != 0 )
-        {
-            if( e.type == SDL_KEYDOWN )
-            {
-                //Select surfaces based on key press
-                switch( e.key.keysym.sym )
-                {
-                    case SDLK_1:
-                        nextAction = '1';
-                    break;
-
-                    case SDLK_2:
-                        nextAction = '2';
-                    break;
-                }
-                quit = true;
-            }
-        }
-    }
-
-    cout << "Welcome" << endl;
-    cout << "Press 1 for a new game" << endl;
-    cout << "Press 2 to load a saved game" << endl;
-    cout << "Press any other key to exit" << endl;
-    cout << "I got : " << nextAction << " boom! " << endl;
-    // TODO:  Add more options? -> Development tools?
+    nextAction = input.WelcomeMenuInput();
 }
 
 Player GameManager::SelectPlayer() {
     Player actualPlayer;        // Return value for player
-
-//    cin >> nextAction;          // Retrieve user entered input
 
     if (nextAction == '2') {
         cout << "Loading an existing game!" << endl;
